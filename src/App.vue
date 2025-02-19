fa63a95f524bcffad6d7586c6e9dd364c098ee5b
DavlatyarovShahzod <davlatyarovshahzod09@gmail.com>
2025-02-19 17:16:48 +0500

    o'zgarish

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <div>
+        <h1>Counter:{{counter}}</h1>
+        <div class ="d-flex">
+            <button v-on:click="onIcrement" class ="btn btn-success">Add</button>
+            <button v-on:click="onDecrement" class ="btn btn-danger">Delete</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            counter:1,
+        }
+    },
+    methods: {
+        onIcrement () {
+            this.counter +=1
+        },
+        onDecrement () {
+            this.counter -=1
+        },
+    },
+}
+</script>
+<style lang="">
+    
+</style>
